735d93d9fe875775c11971dc43730def7f1ab9b6
Ashwini Kumar Mishra <akm@localhost.localdomain>
2020-03-17 15:53:23 +0530

    commit 1

---
+++ building_Module/dashbord.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <script src="../bootstrap/js/jquary.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/dashboard.css">
+  <title>Dashboard</title>
+</head>
+<body>
+  <div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="sidebar">
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
